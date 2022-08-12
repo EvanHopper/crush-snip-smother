f05a272e295f709f8336b679717a96bfc91911c5
Evan Hopper <eahcommercial@gmail.com>
2022-08-12 06:46:26 -0400

    Added basic UI to game.

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+const attacks = document.querySelectorAll('button');
+console.log(attacks);
+attacks.forEach( btn => {
+    btn.addEventListener('click', playRound);
+})
+
+const roundDisplay = document.querySelector('#roundResult');
+
+let score = [0, 0];
+const scoreBoard = document.querySelector('#score');
+setScoreboard();
+
+function setScoreboard() {
+    scoreBoard.textContent = `Meatbag: ${score[0]} | Toaster: ${score[1]}`;
+}
+
+function isGameOver() {
+    if (score[0] == 5 || score[1] == 5) {
+        return true;
+    }
+    else { 
+        return false; 
+    }
+}
+
+function clearBoard() {
+    score.fill(0);
+    setScoreboard();
+    roundDisplay.textContent = '';
+}
+
 const attackArray = ['Crush', 'Snip', 'Smother'];
 
 function getComputerChoice() {
@@ -5,47 +36,75 @@ function getComputerChoice() {
     return attackArray[choice];
 }
 
-function playRound(humanAttack, roboAttack) {
-    switch (humanAttack.toUpperCase()) {
+function getRoundWinner(meatBagChoice) {
+    let roundDetails = {
+        winner: '',
+        message: ''
+    }
+    roboAttack = getComputerChoice();
+
+    switch (meatBagChoice.toUpperCase()) {
         case 'CRUSH':
             if (roboAttack.toUpperCase() === 'SNIP') {
-                return 'Victory: You beat the bot!';
+                roundDetails.winner = 'Meatbag';
+                roundDetails.message = 'Victory: You beat the bot!';
             }
             else if (roboAttack.toUpperCase() === 'SMOTHER') {
-                return 'Defeat: You were smothered by Skynet...';
+                roundDetails.winner = 'Toaster';
+                roundDetails.message = 'Defeat: You were smothered by Skynet...';
             }
             else {
-                return 'Tie: You live to fight another day.';
+                roundDetails.message = 'Tie: You live to fight another day.';
             };
+            break;
         case 'SNIP':
             if (roboAttack.toUpperCase() === 'SMOTHER') {
-                return 'Victory: You are the king of kirigami!';
+                roundDetails.winner = 'Meatbag';
+                roundDetails.message = 'Victory: You are the king of kirigami!';
             }
             else if (roboAttack.toUpperCase() === 'CRUSH') {
-                return 'Defeat: You were brutalized by the bot...';
+                roundDetails.winner = 'Toaster';
+                roundDetails.message = 'Defeat: You were brutalized by the bot...';
             }
             else {
-                return 'Tie: You live to fight another day.';
+                roundDetails.message = 'Tie: You live to fight another day.';
             };
+            break;
         case 'SMOTHER':
             if (roboAttack.toUpperCase() === 'CRUSH') {
-                return 'Victory: Your attack was truly breathtaking!';
+                roundDetails.winner = 'Meatbag';
+                roundDetails.message = 'Victory: Your attack was truly breathtaking!';
             }
             else if (roboAttack.toUpperCase() === 'SNIP') {
-                return 'Defeat: Your electronic enemy tore you to shreads...';
+                roundDetails.winner = 'Toaster';
+                roundDetails.message = 'Defeat: Your electronic enemy tore you to shreads...';
             }
             else {
-                return 'Tie: You live to fight another day.';
+                roundDetails.message = 'Tie: You live to fight another day.';
             };
+            break;
     }
+
+    return roundDetails;
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let meatBagChoice = prompt('Choose your attack!\n(Crush, Snip, or Smother)');
-        let toasterWithAnAttitudeChoice = getComputerChoice();
-        console.log(playRound(meatBagChoice, toasterWithAnAttitudeChoice));
+function playRound(e) {
+    round = getRoundWinner(e.target.id);
+    roundDisplay.textContent = round.message;
+    if (round.winner.toUpperCase() === 'MEATBAG') {
+        score[0] = score[0] + 1;
     }
-
-    console.log('INSERT COIN TO CONTINUE...');
+    else if (round.winner.toUpperCase() === 'TOASTER') {
+        score[1] += 1;
+    }
+    setScoreboard()
+    if (isGameOver()) {
+        if (score[0] > score[1]) {
+            alert("You have won the war!");
+        }
+        else {
+            alert("Humanity is doomed...");
+        }
+        clearBoard();
+    }
 }
